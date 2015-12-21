ed2c0f4cb928371b99f21e91a87817cc1eef38aa
cncgl <concigel@gmail.com>
2015-12-21 23:55:06 +0900

    update boot.js

---
+++ app/vendor.js
@@ -0,0 +1,12 @@
+/**
+ * Created by shigeru on 15/12/21.
+ */
+// require('../node_modules/angular2/bundles/angular2-polyfills');
+// require('../node_modules/rxjs/bundles/Rx');
+require('rxjs');
+// require('../node_modules/angular2/bundles/angular2');
+require('angular2/platform/browser');
+require('angular2/platform/common_dom');
+require('angular2/core');
+require('angular2/router');
+require('angular2/http');
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var path = require('path');
 
 module.exports = {
-  entry: './app/boot.js',
+  entry: {
+    'vendor': './app/vendor.js',
+    'boot': './app/boot.js'
+  },
   output: {
     path: 'build',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   resolve: {
-    modulesDirectories: ['node_modules'],
-    alias: {
-      'angular2': __dirname + '/node_modules/angular2/bundles/angular2-all.umd.js',
-      'polyfills': __dirname + '/node_modules/angular2/bundles/angular2-polyfills.js',
-      'rxjs': __dirname + '/node_modules/rxjs/bundles/Rx.umd.js',
-      'bootstrap': __dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css'
-    }
+    extensions: ['', '.js', '.css', '.html']
+    //modulesDirectories: ['node_modules'],
+    //alias: {
+    //  'angular2': __dirname + '/node_modules/angular2/bundles/angular2-all.umd.js',
+    //  'polyfills': __dirname + '/node_modules/angular2/bundles/angular2-polyfills.js',
+    //  'rxjs': __dirname + '/node_modules/rxjs/bundles/Rx.umd.js',
+    //  'bootstrap': __dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+    //}
   }
 };
